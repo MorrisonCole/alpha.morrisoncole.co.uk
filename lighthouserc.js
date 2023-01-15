9c5945dba12afbbc2a1a45c4282af382ba9ef3b4
Morrison Cole <admin@morrisoncole.co.uk>
2023-01-15 10:26:02 +0900

    fix: lighthouse start command

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,7 +2,7 @@ module.exports = {
   ci: {
     collect: {
       url: ["http://localhost:3000/"],
-      startServerCommand: "yarn serve:next",
+      startServerCommand: "yarn start",
       startServerReadyPattern: "ready - started server",
       numberOfRuns: 1,
     },
