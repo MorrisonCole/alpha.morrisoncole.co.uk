eea928113384e53d6b883d4f9225ad158a79b48a
Morrison Cole <hello@morrisoncole.co.uk>
2024-04-14 09:38:49 +0900

    fix: migrate mdx & eslint

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -2,42 +2,39 @@ import styled from "styled-components";
 import React from "react";
 import Image from "next/image";
 import { Button } from "./components/button";
-import { Components } from "@mdx-js/react/lib";
+import { MDXComponents } from "mdx/types";
 
-const h1 = styled.h1`
-  color: hsl(0, 0%, 50%);
-  font-size: 3em;
-`;
-
-const p = styled.p``;
-
-const ResponsiveImage = ({
-  alt,
-  src,
-  width,
-  height,
-  ...props
-}: Omit<
-  React.DetailedHTMLProps<
-    React.ImgHTMLAttributes<HTMLImageElement>,
-    HTMLImageElement
-  >,
-  "placeholder" | "ref"
->) => (
-  <Image
-    alt={alt ?? "Missing alt"}
-    src={src ?? "Missing"}
-    width={Number(width)}
-    height={Number(height)}
-    placeholder={"blur"}
-    fill
-    {...props}
-  />
-);
-
-export const mdxComponents: Components = {
-  Button: Button,
-  img: ResponsiveImage,
-  h1: h1,
-  p: p,
+export const useMDXComponents = (components: MDXComponents): MDXComponents => {
+  return {
+    Button: Button,
+    img: ({
+      alt,
+      src,
+      width,
+      height,
+      ...props
+    }: Omit<
+      React.DetailedHTMLProps<
+        React.ImgHTMLAttributes<HTMLImageElement>,
+        HTMLImageElement
+      >,
+      "placeholder" | "ref"
+    >) => (
+      <Image
+        alt={alt ?? "Missing alt"}
+        src={src ?? "Missing"}
+        width={Number(width)}
+        height={Number(height)}
+        placeholder={"blur"}
+        fill
+        {...props}
+      />
+    ),
+    h1: styled.h1`
+      color: hsl(0, 0%, 50%);
+      font-size: 3em;
+    `,
+    p: styled.p``,
+    ...components,
+  };
 };
