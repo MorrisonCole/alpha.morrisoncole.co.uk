38e400d4885cf661c404691ebfd4faca2b5ec4a8
Morrison Cole <hello@morrisoncole.co.uk>
2024-04-14 14:48:36 +0900

    chore: use recommended styledcomponents integration

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,35 +1,8 @@
-import Document, {
-  DocumentContext,
-  Head,
-  Html,
-  Main,
-  NextScript,
-} from "next/document";
+import StyledComponentsRegistry from "@/lib/registry";
+import Document, { Head, Html, Main, NextScript } from "next/document";
 import React from "react";
-import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    const sheet = new ServerStyleSheet();
-    const originalRenderPage = ctx.renderPage;
-
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
-        });
-
-      const initialProps = await Document.getInitialProps(ctx);
-      return {
-        ...initialProps,
-        styles: [initialProps.styles, sheet.getStyleElement()],
-      };
-    } finally {
-      sheet.seal();
-    }
-  }
-
   render() {
     const setColorsByTheme = `
       function getUserPreference() {
@@ -76,7 +49,9 @@ export default class MyDocument extends Document {
         </Head>
         <body>
           <script dangerouslySetInnerHTML={{ __html: setColorsByTheme }} />
-          <Main />
+          <StyledComponentsRegistry>
+            <Main />
+          </StyledComponentsRegistry>
           <NextScript />
         </body>
       </Html>
