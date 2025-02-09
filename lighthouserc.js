2e93a053dcaf86313b0a95e7fb4326e14ee9cfcc
Morrison Cole <hello@morrisoncole.co.uk>
2025-02-09 09:54:14 +0900

    fix: convert to es module

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,25 +1,23 @@
-module.exports = {
-  ci: {
-    collect: {
-      url: ["http://localhost:3000/en"],
-      startServerCommand: "yarn start",
-      startServerReadyPattern: "ready - started server",
-      numberOfRuns: 1,
-    },
-    upload: {
-      target: "temporary-public-storage",
-    },
-    assert: {
-      preset: "lighthouse:no-pwa",
-      assertions: {
-        "bf-cache": "warn",
-        "color-contrast": "warn",
-        "csp-xss": "warn",
-        "is-crawlable": "warn",
-        "total-byte-weight": "warn",
-        "unused-javascript": "warn",
-        "unused-css-rules": "warn",
-      },
+export const ci = {
+  collect: {
+    url: ["http://localhost:3000/en"],
+    startServerCommand: "yarn start",
+    startServerReadyPattern: "ready - started server",
+    numberOfRuns: 1,
+  },
+  upload: {
+    target: "temporary-public-storage",
+  },
+  assert: {
+    preset: "lighthouse:no-pwa",
+    assertions: {
+      "bf-cache": "warn",
+      "color-contrast": "warn",
+      "csp-xss": "warn",
+      "is-crawlable": "warn",
+      "total-byte-weight": "warn",
+      "unused-javascript": "warn",
+      "unused-css-rules": "warn",
     },
   },
 };
