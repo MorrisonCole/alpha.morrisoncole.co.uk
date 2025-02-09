477674ad5752516c0a4e6e06d4158955aa4eef58
Morrison Cole <hello@morrisoncole.co.uk>
2025-02-09 10:05:22 +0900

    fix: module

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,23 +1,25 @@
-export const ci = {
-  collect: {
-    url: ["http://localhost:3000/en"],
-    startServerCommand: "yarn start",
-    startServerReadyPattern: "ready - started server",
-    numberOfRuns: 1,
-  },
-  upload: {
-    target: "temporary-public-storage",
-  },
-  assert: {
-    preset: "lighthouse:no-pwa",
-    assertions: {
-      "bf-cache": "warn",
-      "color-contrast": "warn",
-      "csp-xss": "warn",
-      "is-crawlable": "warn",
-      "total-byte-weight": "warn",
-      "unused-javascript": "warn",
-      "unused-css-rules": "warn",
+module.exports = {
+  ci: {
+    collect: {
+      url: ["http://localhost:3000/en"],
+      startServerCommand: "yarn start",
+      startServerReadyPattern: "ready - started server",
+      numberOfRuns: 1,
+    },
+    upload: {
+      target: "temporary-public-storage",
+    },
+    assert: {
+      preset: "lighthouse:no-pwa",
+      assertions: {
+        "bf-cache": "warn",
+        "color-contrast": "warn",
+        "csp-xss": "warn",
+        "is-crawlable": "warn",
+        "total-byte-weight": "warn",
+        "unused-javascript": "warn",
+        "unused-css-rules": "warn",
+      },
     },
   },
 };
