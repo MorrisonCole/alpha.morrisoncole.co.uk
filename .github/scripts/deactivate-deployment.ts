915b7ad448d2ead8f09c3f685b495c1e122ddfdc
Morrison Cole <hello@morrisoncole.co.uk>
2026-03-28 10:27:24 +0900

    feat: add github deployments (#923)
    * feat: add github deployments
    
    * fix: don't ignore scripts
    
    * chore: node 25
    
    * chore: node 25
    
    * fix: remove jest types

---
+++ .github/scripts/deactivate-deployment.ts
@@ -0,0 +1,30 @@
+import { getOctokit, context } from "@actions/github";
+
+const octokit = getOctokit(process.env["GITHUB_TOKEN"]!);
+const { owner, repo } = context.repo;
+const environment = `pr-${process.env["PR_NUMBER"]}`;
+
+const { data: deployments } = await octokit.rest.repos.listDeployments({
+  owner,
+  repo,
+  environment,
+});
+
+for (const deployment of deployments) {
+  await octokit.rest.repos.createDeploymentStatus({
+    owner,
+    repo,
+    deployment_id: deployment.id,
+    state: "inactive",
+  });
+}
+
+await octokit.rest.repos.deleteAnEnvironment({
+  owner,
+  repo,
+  environment_name: environment,
+});
+
+console.log(
+  `Deactivated ${deployments.length} deployment(s) and deleted environment "${environment}"`,
+);
